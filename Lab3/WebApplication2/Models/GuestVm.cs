8db81c54f532c6bb9a942f545e0789122997b205
MaxKravchuk <54956796+MaxKravchuk@users.noreply.github.com>
2022-01-23 16:46:44 +0200

    Web Api

---
+++ Lab3/WebApplication2/Models/GuestVm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class GuestVm
+    {
+        public string Login { get; set; }
+        public string PasswordHash { get; set; }
+
+        public string GuestRole { get; set; }
+
+    }
+}
